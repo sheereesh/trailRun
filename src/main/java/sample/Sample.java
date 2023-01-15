50873ffd04c2013e4fd5853f434df4ced28dbabb
sheereesh <alekhy99@gmail.com>
2023-01-15 22:47:11 +0530

    changed

--- src/main/java/sample/Sample.java
+++ src/main/java/sample/Sample.java
@@ -4,6 +4,7 @@ public class Sample {
 public static void main(String[] args) {
 	System.out.println("im the one");
 	System.out.println("git hub checking kjhkgkjgkkkkjblkkl");
+	System.out.println("lkalfjalsflsofjasfd ");
 
 	
 	
